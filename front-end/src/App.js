2b02132e8d6c51b854dab77f08980e93511b98e9
William W. Huang <williamhuang0623@gmail.com>
2021-03-14 19:58:07 -0400

    i suck

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Practice from './page/Practice';
+import Learn from './page/Learn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* Nav goes here */}
-      </header>
-    </div>
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/practice">
+              <Practice />
+            </Route>
+            <Route path="/learn">
+              <Learn />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
