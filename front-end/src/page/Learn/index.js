d98c87e8d2d1ff6e7bedbc04870e08d1ea7bdb82
William W. Huang <williamhuang0623@gmail.com>
2021-03-14 20:07:24 -0400

    family friendly repo

---
+++ front-end/src/page/Learn/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Learn extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                Learn Page
+            </div>
+        )
+    }
+}
+
+export default Learn;
---
+++ front-end/src/page/Practice/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Practice extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                Practice
+            </div>
+        )
+    }
+}
+
+export default Practice;
