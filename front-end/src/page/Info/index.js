ce5815404486eb5fc6b57b6c0dba3edc780f7faf
jw5374 <jw5374@nyu.edu>
2021-03-18 20:03:37 -0400

    Info page with semantic-ui

--- front-end/src/page/Info/index.js
+++ front-end/src/page/Info/index.js
@@ -1,14 +1,162 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { Grid, Menu, Icon, Sidebar, Image, Tab } from 'semantic-ui-react';
+import './Info.css';
+const imgsrc = '/img/chesslogo.png';
 class Info extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            visible: false,
+            activeItem: 'history',
+            activePane: false
+        }
     }
-    
+    toggleVisible() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
+
+    togglePane() {
+        this.setState({
+            activePane: !this.state.activePane
+        })
+    }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
     render() {
+        const { activeItem } = this.state
+        const panes = [
+            {menuItem: 
+                <Menu.Item
+                style={{color: 'lightgrey'}}
+                name='history'
+                active={activeItem === 'history'}
+                onClick={this.handleItemClick}
+                color='teal'
+                />
+            , render: () => 
+            <Tab.Pane style={{backgroundColor: '#00141b', color: 'lightgrey'}}>
+                Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text  
+                <br />Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text Some Info Text 
+                <br />Some Info Text Some Info Text 
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+                <br />Some Info Text
+            </Tab.Pane>},
+            {menuItem: 
+                <Menu.Item
+                style={{color: 'lightgrey'}}
+                name='notable players'
+                active={activeItem === 'notable players'}
+                onClick={this.handleItemClick}
+                color='teal'
+                />
+            , render: () => 
+            <Tab.Pane style={{backgroundColor: '#00141b', color: 'lightgrey'}}>
+                Some Info Text Some Info Text 
+            </Tab.Pane>},
+            {menuItem:  
+                <Menu.Item
+                style={{color: 'lightgrey'}}
+                name='Recent Events'
+                active={activeItem === 'Recent Events'}
+                onClick={this.handleItemClick}
+                color='teal'
+                />
+            , render: () => 
+            <Tab.Pane style={{backgroundColor: '#00141b', color: 'lightgrey'}}>
+                Some Info Text Some Info Text Some Info Text
+            </Tab.Pane>},
+            {menuItem: 
+                <Menu.Item
+                style={{color: 'lightgrey'}}
+                name='Resources'
+                active={activeItem === 'Resources'}
+                onClick={this.handleItemClick}
+                color='teal'
+                />
+            , render: () => 
+            <Tab.Pane style={{backgroundColor: '#00141b', color: 'lightgrey'}}>
+                Some Info Text Some Info Text Some Info Text Some Info Text
+                <br />
+                <a href="https://en.wikipedia.org/wiki/Chess">Hey it's a link</a>
+            </Tab.Pane>}
+        ]
         return (
             <div>
-                Info Page
+                <div className="stretch">
+                    <div style={{flex: 1}}>
+                        <Sidebar.Pushable style={{backgroundColor: '#00141b'}}>
+                            <Sidebar
+                                as={Menu}
+                                animation='overlay'
+                                icon='labeled'
+                                inverted
+                                onHide ={() => {this.toggleVisible()}}
+                                vertical
+                                visible={this.state.visible}
+                                width='thin'
+                                direction='right'
+                            >
+                                <Menu.Item as={Link} to='/'>
+                                <Icon name='chess board' />
+                                Play
+                                </Menu.Item>
+                                <Menu.Item as={Link} to='/learn'>
+                                <Icon name='pencil square' />
+                                Learn
+                                </Menu.Item>
+                                <Menu.Item as={Link} to='/practice'>
+                                <Icon name='chess' />
+                                Practice
+                                </Menu.Item>
+                                <Menu.Item as={Link} to='/info'>
+                                <Icon name='question' />
+                                Info
+                                </Menu.Item>
+                            </Sidebar>
+                            
+                            <Sidebar.Pusher dimmed={this.state.visible}>
+                                <Grid style={{color: 'lightgrey'}}>
+                                    <Grid.Row>
+                                        <Grid.Column floated='right'>
+                                            <Image src={imgsrc} size='medium' verticalAlign='top' floated='right' onClick={() => {
+                                            this.toggleVisible();
+                                        }}/>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                    <Grid.Row></Grid.Row>
+                                </Grid>
+                                <Tab 
+                                    style={{height: '500px'}} 
+                                    grid={{paneWidth: 13, tabWidth: 2}} 
+                                    menu={{inverted: true, fluid: true, vertical: true, tabular: true, pointing: true, menuPosition: 'left'}}
+                                    panes={panes} 
+                                />
+                            </Sidebar.Pusher>
+                        </Sidebar.Pushable>
+                    </div>
+                </div>
             </div>
         )
     }
